facf856a0e9d4241af9e4eee0e25d126c8c23a34
S.O.V.A <headshot2yourself@gmail.com>
2024-10-10 15:43:16 +0300

    First Lesson

---
+++ src/HelloMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HelloMessage() {
+	return (
+		<div>
+			<h2>Привет от нового компонента!</h2>
+			<p>Этот компонент был подключен отдельно.</p>
+		</div>
+
+	);
+}
+
+export default HelloMessage;
