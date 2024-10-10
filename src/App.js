986b1bbaafb47ef2772c6a72bdb2de26ceb92b12
S.O.V.A <headshot2yourself@gmail.com>
2024-10-10 15:50:35 +0300

    Практическое задание выполнено

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HelloMessage from './HelloMessage';
+import PeopleList from './PeopleList';
 
 function App() {
     return (
@@ -7,6 +8,7 @@ function App() {
             <h1>Привет, React!</h1>
             <p>Это мой первый компонент.</p>
             <HelloMessage />
+            <PeopleList />
         </div>
     );
 }
---
+++ src/PeopleList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function PeopleList() {
+    return (
+        <div>
+            <ul>
+                <h3>Список людей отдельный компонентом</h3>
+                <li>Вова</li>
+                <li>Анна</li>
+                <li>Иван</li>
+                <li>Фео</li>
+            </ul>
+        </div>
+    );
+}
+
+export default PeopleList;
